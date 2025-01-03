38c3b093967c0d7f4e67b162fb8eca04bddc8c89
Jan Brejnik <jan.brejnik@aevi.com>
2025-01-03 14:02:32 +0100

    Change text

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 void buggyFunction() {
     char buffer[10];
-    std::strcpy(buffer, "This is a very long string that will overflow the buffer");
+    std::strcpy(buffer, "This is a very long string that will overflow the buffer... oops!");
 }
 
 int main() {
