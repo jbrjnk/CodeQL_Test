c8e95f2fcd14785b580200548fd06143b1bab4bb
Jan Brejnik <jan.brejnik@aevi.com>
2025-01-15 18:36:48 +0100

    Commit

--- main.cpp
+++ main.cpp
@@ -6,17 +6,12 @@
 // Comment 3
 // Comment 4
 
-void buggyFunction() {
-    char buffer[10];
-    std::strcpy(buffer, "This is a very long string that will overflow the buffer");
-}
-
 void buggyFunction2() {
     char buffer[10];
     std::strcpy(buffer, "This is a very long string that will overflow the buffer");
 }
 
 int main() {
-    buggyFunction();
+    buggyFunction2();
     return 0;
 }
