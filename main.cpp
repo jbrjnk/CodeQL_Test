dca36a0a44a8cb5606491d4d38a0cbef01d6fc19
Jan Brejnik <jan.brejnik@aevi.com>
2025-01-15 20:47:50 +0100

    Another commit

--- main.cpp
+++ main.cpp
@@ -8,10 +8,18 @@
 
 void buggyFunction2() {
     char buffer[10];
-    std::strcpy(buffer, "This is a very long string that will overflow the buffer");
+    std::strcpy(buffer, "This is a very long string that will overflow the buffer xxyyzz");
+}
+
+void useAfterFree() {
+    int* ptr = new int(10);
+    delete ptr;
+    std::cout << *ptr << std::endl; // Use after free
 }
 
 int main() {
+    std::cout << "Hello, World!" << std::endl;
     buggyFunction2();
+    useAfterFree();
     return 0;
 }
