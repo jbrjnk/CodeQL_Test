ec20e597aece29e161242f1c1c66e908cb5eca75
Jan Brejnik <jan.brejnik@aevi.com>
2025-01-15 15:25:42 +0100

    Comment added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstring>
 
+// Comment
+
 void buggyFunction() {
     char buffer[10];
     std::strcpy(buffer, "This is a very long string that will overflow the buffer");
