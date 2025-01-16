65541426c62659d84496d3d482b71b52c14bbe25
Jan Brejnik <jan.brejnik@aevi.com>
2025-01-16 21:05:38 +0100

    Another commit

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 void buggyFunction2() {
     char buffer[10];
-    std::strcpy(buffer, "This is a very long string that will overflow the buffer xxyyzz");
+    std::strcpy(buffer, "This is a very long string that will overflow the buffer.");
 }
 
 void DoSomething()
